6d5245f4e19c613aea15a6e39af13ceb1f807277
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-08-10 08:51:35 +0200

    add map part

--- src/main/java/com/klodnicki/collections/Main.java
+++ src/main/java/com/klodnicki/collections/Main.java
@@ -49,19 +49,45 @@ public class Main {
         System.out.println(Arrays.toString(numbers3));
 
 
-    }
-
-    //-----------------------------------------------------------------------------------------------------------------------
-    //ARRAYLIST
-    ArrayList<Integer> numbersArrList = new ArrayList<>();
+        //-----------------------------------------------------------------------------------------------------------------------
+        //ARRAYLIST
+        ArrayList<Integer> numbersArrList = new ArrayList<>();
         numbersArrList.add(1);
         System.out.println(numbersArrList.size());
 
-    // Castowanie
-    long smallNumber = 3_000_000_000L;
-    int anotherNumber = (int) smallNumber;
-    // min int = -2_147_483_646
-    // max int = 2_147_483_647
+        // Castowanie
+        long smallNumber = 3_000_000_000L;
+        int anotherNumber = (int) smallNumber;
+        // min int = -2_147_483_646
+        // max int = 2_147_483_647
+
+        //----------------------------------------------------------------------------------------------------------------------
+        //MAP
+
+        // Map to interfejs, a HashMap, TreeMap, LinkedHashMap to konkretne implementacje
+        // I tak samo dla List. List to interfejs, a ArrayList, LinkedList to konkretne implementacje.
+        // Set to interfejs, a HashSet, TreeSet to konkretna implementacja
+
+        Map<String, Double> menu = new HashMap<>();  //Map stosuje sie gdy potrzebujesz pary wartości.
+        // Set jesli jest taki sam to nie wydrukuje się. Set i List jest bardzo podobny, Set sie preferuje, zeby nie bylo duplikatów.
+        menu.put("Mojito", 20.0);
+        menu.put(null, 18.0);
+        menu.put("baaa", 20.0);
+        menu.put("Sex on the Beach", 18.0);
+
+        menu.putIfAbsent(null, 20.0);
+
+        System.out.println(menu.containsKey("baaa"));
+        System.out.println(menu.containsValue(100.0));
+
+        System.out.println(menu.remove("Mojito"));
+        System.out.println(menu.remove("Tego nie ma"));
+
+        menu.putIfAbsent("Mojito", 10.0);
+
+        System.out.println(menu);
 
 
+    }
 }
+
