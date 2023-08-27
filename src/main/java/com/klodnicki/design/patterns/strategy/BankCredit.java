9b28a98fb36779dafdd6cfdd6f4ff1cd602d2fca
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-08-27 10:36:52 +0200

    add design pattern Strategy -> first payment strategy bank credit

---
+++ src/main/java/com/klodnicki/design/patterns/strategy/BankCredit.java
@@ -0,0 +1,16 @@
+package com.klodnicki.design.patterns.strategy;
+
+public class BankCredit implements PaymentStrategy{
+
+    private String name;
+    private String bankName;
+
+    public BankCredit(String name, String bankName) {
+        this.name = name;
+        this.bankName = bankName;
+    }
+    @Override
+    public void pay(int amount) {
+        System.out.println(name + " zapłacił kredytem z banku: " + bankName);
+    }
+}
