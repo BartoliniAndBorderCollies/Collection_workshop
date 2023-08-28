13105b558d416d065be5a11d51fa9464dea4189c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-08-28 18:39:59 +0200

    add factory design lesson -> GoCart class

---
+++ src/main/java/com/klodnicki/design/patterns/factory/lesson/GoCart.java
@@ -0,0 +1,8 @@
+package com.klodnicki.design.patterns.factory.lesson;
+
+public class GoCart implements Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Bruuuum");
+    }
+}
