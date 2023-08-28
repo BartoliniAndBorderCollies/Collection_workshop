46f4a97a8e0f170fd02d77f58b52f4a24735cd1c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-08-28 18:40:55 +0200

    add factory design lesson -> Truck class

---
+++ src/main/java/com/klodnicki/design/patterns/factory/lesson/Truck.java
@@ -0,0 +1,10 @@
+package com.klodnicki.design.patterns.factory.lesson;
+
+public class Truck implements Vehicle {
+    private String color;
+
+    public void drive() {
+        // implementation
+        System.out.println("Driving on the road.");
+    }
+}
